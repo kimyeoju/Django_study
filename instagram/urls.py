028425de25a7c615b7b383c2b918b7c0fbb3e9e8
kimyeoju <yeojoo0031@gmail.com>
2023-11-26 17:30:25 +0900

    feat : 커스텀 Path Converter 코드 추가

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,28 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+
+class YearConverter:
+    regex = r"20\d{2}"
+    
+    # url 매칭이 되었을 때 views.archives_year 함수 호출하기 전에 인자를 한번 정리
+    # 정규표현식으로 인자를 뽑아내면 그 형태는 문자열인데, int 정수로 변환해서 넘겨준다.
+    def to_python(self, value):
+        return int(value)
+    
+    # 어떤 값을 url 문자열로 리버싱할 때 호출되는 함수
+    # int -> str로 변환 
+    def to_url(self, value):
+        return str(value)
+        # return "%04d" % value
+
+
+register_converter(YearConverter, 'converter_year')
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
+    # path('archives/<int:year>/', views.archives_yesr),
+    # re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
+    path('archives/<converter_year:year>/', views.archives_year),
+    
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,12 +1,28 @@
+from django.views.generic import ListView
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Post
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q':q,
-    })
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q':q,
+#     })
+
+
+def post_detail(request, pk):
+    response = HttpResponse()
+    response.write("Hello World")
+    response.write("Hello World")
+    response.write("Hello World")
+    return Response
+
+
+def archives_year(request, year):
+    return HttpResponse(f'{year}년 archives')
