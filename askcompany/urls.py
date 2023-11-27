2f165fbb978a9e8ae79a92ffec2204d7fffa500c
kimyeoju <yeojoo0031@gmail.com>
2023-11-28 01:41:14 +0900

    feat : 최상위 url 코드 추가

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -5,8 +5,17 @@ from django.urls import path, include
 # from django.conf import global_settings
 # from askcompany import settings
 from django.conf import settings
+from django.views.generic import TemplateView, RedirectView
+
+# class RootView(TemplateView):
+#     template_name = 'root.html'
 
 urlpatterns = [
+    # path('', TemplateView.as_view(template_name='root.html'), name='root'),
+    path('', RedirectView.as_view(
+        # url='/instagram/'
+        pattern_name = 'instagram:post_list',
+        ), name='root'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
