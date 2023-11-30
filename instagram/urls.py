d5754f2b673ce197442c73a1dfbbd4594c316953
kimyeoju <yeojoo0031@gmail.com>
2023-11-30 18:00:47 +0900

    feat : MonthArchiveView, DayArchiveView 추가 및 converter.py 폴더 생성

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,24 +1,12 @@
 from django.urls import path, re_path, register_converter
 from . import views
 
-class YearConverter:
-    regex = r"20\d{2}"
-    
-    # url 매칭이 되었을 때 views.archives_year 함수 호출하기 전에 인자를 한번 정리
-    # 정규표현식으로 인자를 뽑아내면 그 형태는 문자열인데, int 정수로 변환해서 넘겨준다.
-    # url로부터 추출한 문자열을 뷰에 넘겨주기 전에 변환
-    def to_python(self, value):
-        return int(value)
-    
-    # 어떤 값을 url 문자열로 리버싱할 때 호출되는 함수
-    # int -> str로 변환
-    # url reverse 시에 호출
-    def to_url(self, value):
-        return str(value)
-        # return "%04d" % value
 
+from .converters import YearConverter, MonthConverter, DayConverter
 
 register_converter(YearConverter, 'converter_year')
+register_converter(MonthConverter, 'converter_month')
+register_converter(DayConverter, 'converter_day')
 
 app_name = 'instagram' # URL Reverse에서 namespace 역할을 하게 된다.
 
@@ -29,5 +17,11 @@ urlpatterns = [
     # re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
     # path('archives/<converter_year:year>/', views.archives_year),
     path('archive/', views.post_archive, name='post_archive'),
+    # 년도(year)
     path('archive/<converter_year:year>/', views.post_year_archive, name='post_year_archive'),
+    # 달 (month)
+    # path('archive/<converter_year:year>/<converter_month:month>/', views.post_m,onth_archive, name='post_month_archive'),
+    # 일 (day)
+    # path('archive/<converter_year:year>/<converter_month:month>/<converter_day:day>/', views.post_day_archive, name='post_day_archive'),
+    
 ]
