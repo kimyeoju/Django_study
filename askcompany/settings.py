1ae6879abbbbe48789787b4cf96345a86be10943
kimyeoju <yeojoo0031@gmail.com>
2023-12-08 15:41:44 +0900

    chore : settings.py static 관련 코드 추가

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
+]
 
 MEDIA_URL = '/media/' # 미디어 파일에 접근하기 위한 URL
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 파일이 저장된 로컬 파일 시스템의 경로 
