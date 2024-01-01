1101151cc3305403d0c99fadae11db4eca9b69bb
kimyeoju <yeojoo0031@gmail.com>
2024-01-01 23:25:23 +0900

    feat : profile 클래스뷰 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+profile = ProfileView.as_view()
