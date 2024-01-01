6f3507bc29e30aa34db26b547fa1a911ec2e57fb
kimyeoju <yeojoo0031@gmail.com>
2024-01-02 00:06:21 +0900

    feat : profile 수정 views 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,51 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from accounts.forms import ProfileForm
+from accounts.models import Profile
+from django.shortcuts import redirect
 
+
+
+# 선택 1
 # @login_required
 # def profile(request):
 #     return render(request, 'accounts/profile.html')
 
+
+# 선택 2
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
 
 profile = ProfileView.as_view()
+
+
+# class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Profile
+#     form_class = ProfileForm
+    
+# profile_edit = ProfileUpdateView.as_view()
+
+
+@login_required
+def profile_edit(request):
+    try:
+        # 프로필이 있는지 확인
+        profile = request.user.profile
+    # Profile.objects.get(user=request.user) 위의 코드랑 같은 코드
+    except Profile.DoesNotExist:
+        profile = None
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form' : form,
+    })
